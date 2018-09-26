cad774639d82ccb4f57db1ee778593d0ee17809f
linsen <3057754733@qq.com>
2018-09-26 23:08:27 +0800

    init project

---
+++ app/src/main/java/com/example/linsen/weather/Weather.java
@@ -0,0 +1,17 @@
+package com.example.linsen.weather;
+
+import android.app.Activity;
+import android.os.Bundle;
+/**
+ * Created by linsen on 2018/9/26.
+ */
+
+public class Weather extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+
+}
+
